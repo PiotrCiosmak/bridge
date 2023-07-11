42da06d9533fb4a4c5849059d3d9309f6bf38811
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-07-11 16:29:35 +0200

    updated

--- include/devices/Device.hpp
+++ include/devices/Device.hpp
@@ -3,7 +3,7 @@
 class Device
 {
 public:
-    [[nodiscard]] virtual int getVolume() = 0;
+    virtual int getVolume() = 0;
     virtual void setVolume(int volume) = 0;
     virtual ~Device() = default;
 protected:
